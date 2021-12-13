f678fa1e2755ed32f270f1119ec96517a42b432c
Shashi Bawa <shashibawa104@gmail.com>
2021-12-13 19:18:59 +0000

    Implemented logic

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) return true;
     }
-}
+    for (let j = 0; j < letters[0].length; j++) {
+      let str = "";
+  
+      for (let i = 0; i < letters.length; i++) {
+        str += letters[i][j];
+      }
+  
+      if (str.includes(word)) {
+        return true;
+      }
+    }
+    return false;
+  };
+  
 
 module.exports = wordSearch
